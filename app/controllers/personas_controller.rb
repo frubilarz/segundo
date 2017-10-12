ab27335db657a568ac1421c351d114926e3f6200
Fernando P. Rubilar Z <frubilar@icci.cl>
2017-10-12 12:27:54 -0300

    CHG: controlador personas [eliminar, update, crear] add: strong_params en controlador personas add: before_action en controlador personas vistas de formulario y varias
    Signed-off-by: Fernando P. Rubilar Z <frubilar@icci.cl>

--- app/controllers/personas_controller.rb
+++ app/controllers/personas_controller.rb
@@ -1,17 +1,29 @@
 class PersonasController < ApplicationController
+  before_action :set_persona, only: [:mostrar, :editar, :update,:eliminar]
 
   def index
     @personas = Persona.all
   end
 
   def mostrar
-    @persona = Persona.find(params[:id])
   end
 
   def eliminar
+    @persona.destroy
+    respond_to do |format|
+      format.html{redirect_to @personas, notice: 'eliminado'}
+    end
+
   end
 
   def update
+    respond_to do |format|
+      if @persona.update(persona_params)
+        format.html {redirect_to persona_path(@persona), notice: 'Se Actualizaron los datos'}
+      else
+        render :editar
+      end
+    end
   end
 
   def nuevo
@@ -19,8 +31,27 @@ class PersonasController < ApplicationController
   end
 
   def crear
+    @persona = Persona.new(persona_params)
+    respond_to do |format|
+      if @persona.save
+        format.html {redirect_to persona_path(@persona), notice: 'Se Persistio la persona'}
+      else
+        format.html {render :nuevo}
+      end
+    end
   end
 
   def editar
   end
+
+  private
+
+  def set_persona
+    @persona = Persona.find(params[:id])
+  end
+
+  def persona_params
+    params.require(:persona).permit(:nombres, :apellidos, :fecha_nacimiento, :direccion)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
   get 'personas/', to: 'personas#index', as: 'personas'
-
-
-  get 'personas/eliminar'
-
-  get 'personas/update'
-
-  get 'personas/nuevo'
-
+  get 'personas/nuevo', to: 'personas#nuevo', as: 'nueva_persona'
+  post '/personas', to: 'personas#crear', as: 'crear_persona'
+  put 'personas/:id', to: 'personas#update'
+  patch 'personas/:id', to: 'personas#update'
   get 'personas/crear'
 
-  get 'personas/editar'
   get 'personas/:id', to: 'personas#mostrar', as: 'persona'
+  get 'personas/:id/editar', to: 'personas#editar', as: 'editar_persona'
+  delete 'personas/:id', to: 'personas#eliminar'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
